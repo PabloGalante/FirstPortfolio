83f42c120c9cdd889374ae7ce7a01cdcc8ad928a
Pablo Galante <pablogalante95@gmail.com>
2021-12-28 15:10:34 -0300

    Finish Project (#1)

--- resources/js/script.js
+++ resources/js/script.js
@@ -1,3 +1,12 @@
+function menu() {
+    const x = document.getElementById("navBarId");
+    if (x.className === "navBar") {
+      x.className += " responsive";
+    } else {
+      x.className = "navBar";
+    }
+  }
+
 let id = (id) => document.getElementById(id);
 
 let classes = (classes) => document.getElementsByClassName(classes);
@@ -31,12 +40,3 @@ let engine = (id, serial, message) => {
         successIcon[serial].style.opacity = "1";
     }
 }
-
-function menu() {
-    var x = document.getElementById("navBarId");
-    if (x.className === "navBar") {
-      x.className += " responsive";
-    } else {
-      x.className = "navBar";
-    }
-  }
