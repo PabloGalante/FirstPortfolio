be7ccb41ed92e22ec39e003a5aa117d94e4d6b22
Pablo Galante <pablogalante95@gmail.com>
2022-07-15 11:52:20 -0300

    Form validation logic

--- resources/js/emailJS.js
+++ resources/js/emailJS.js
@@ -6,16 +6,52 @@ const templateid = config.TEMPLATE_ID_EMAILJS;
     emailjs.init(userid);
 })();
 
+function emailRegex(mail){
+    const regex = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/;
+
+    return regex.test(mail) ? true : false;
+}
+
+function checker(name, email, msg){
+    let allGood = false;
+
+    let nameOk = false;
+    let emailOk = false;
+    let msgOk = false;
+
+    let mailCheck = emailRegex(email);
+
+    name != "" && name.length < 50 ? nameOk = true : nameOk = false;
+
+    email != 0 && mailCheck ? emailOk = true : emailOk = false;
+
+    msg != "" && msg.length < 300 ? msgOk = true : msgOk = false;
+
+    console.log(`nameOk: ${nameOk}, emailOk: ${emailOk}, msgOk: ${msgOk}, mailCheck: ${mailCheck}, allGood:${allGood}`)
+
+    if(nameOk && emailOk && msgOk){
+        allGood = true;
+        return allGood;
+    }
+};
+
 function sendmail() {
     let fullName = document.getElementById("username").value;
     let userEmail = document.getElementById("email").value;
     let userMessage = document.getElementById("textarea").value;
+    let submitButton = document.getElementById("btn");
 
-        const contactParams = {
-            from_name: fullName,
-            from_email: userEmail,
-            message: userMessage
-        };
+    const contactParams = {
+        from_name: fullName,
+        from_email: userEmail,
+        message: userMessage
+    };
+    
+    const checkValue = checker(fullName, userEmail, userMessage);
 
-    emailjs.send(serviceid, templateid, contactParams).then(function (res) {})
+    if(checkValue){
+        emailjs.send(serviceid, templateid, contactParams).then(function (res) {})
+    }else{
+        alert('Verifica que los campos sean validos');
+    }
 };
